22918c16a819a200e183b1555b6ebc6fa203a5c5
Alex Stephen <astephen2@gmail.com>
2024-11-16 13:21:44 -0800

    delete works!

--- src/app/explorer/resource_list.tsx
+++ src/app/explorer/resource_list.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/table";
 import { ColumnDef } from "@tanstack/react-table"
 import { useSpec } from "@/state/StateContext";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { DataTable } from "@/components/ui/data-table";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
@@ -16,86 +16,94 @@ import { Button } from "@/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
 import { toast } from "@/hooks/use-toast";
 import { ResourceSchema } from "@/state/openapi";
-
-const dropDownMenuColumn = {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-        const resource = row.original
-
-        return (
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                        <span className="sr-only">Open menu</span>
-                        <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuItem
-                        onClick={() => toast({ description: `Deleting ${resource}` })}
-                    >
-                        <span className="text-red-600">Delete</span>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        )
-    },
-};
-
-
-
-function createColumns(r: ResourceSchema): object[] {
-    if(r) {
-        return r.properties().map((prop) => {
-            return {accessorKey: prop.name, header: prop.name}
-        });
-    } else {
-        return [];
-    }
-}
+import { ResourceInstance } from "@/state/fetch";
 
 type ResourceListState = {
-    results: object[],
-    columns: object[]
+    resources: ResourceInstance[],
 }
 
 export default function ResourceList() {
     const { resourceId } = useParams();
     const { spec, setSpec } = useSpec();
 
+    const dropDownMenuColumn = {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+            const resource = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem
+                            onClick={() => deleteResource(resource)}
+                        >
+                            <span className="text-red-600">Delete</span>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    };
+
+    function createColumns(r: ResourceSchema | undefined): object[] {
+        if(r) {
+            let columns = r.properties().map((prop) => {
+                return {accessorKey: prop.name, header: prop.name}
+            });
+            columns.push(dropDownMenuColumn);
+            return columns;
+        } else {
+            return [];
+        }
+    }
+
+
+
     const [state, setState] = useState<ResourceListState>(
         {
-            results: [{id: "Loading..."}],
-            columns: [{accessorKey: "id", header: "id"}],
+            resources: [],
         });
 
-    useEffect(() => {
+    function deleteResource(r: object) {
+        const result = state?.resources.find((result: ResourceInstance) => result.id === r.id);
+        if(result) {
+            result.delete().then(() => {
+                toast({description: `Deleted ${result.path}`});
+                refreshList();
+            })
+        }
+    }
+    const refreshList = useCallback(() => {
         if (spec && resourceId) {
             spec!
                 .resourceForName(resourceId)
                 .list()
                 .then((resources) => {
                     if (resources) {
-                        let resource_data = resources.map((resource) => resource.properties);
-                        console.log(resource_data);
-                        let column_list: object[] = createColumns(spec!.resourceForName(resourceId));
-                        column_list.push(dropDownMenuColumn);
-                        console.log(column_list);
                         setState({
-                            results: resource_data,
-                            columns: column_list,
+                            resources: resources,
                         });
                     }
                 });
         }
-    }, [spec, resourceId]);
+    }, [spec, resourceId]);  // Add dependencies
+
+    useEffect(() => {
+        refreshList();
+    }, [spec, resourceId, refreshList]);
 
     return (
         <div>
             <h1>{resourceId}</h1>
 
-            <DataTable columns={state.columns} data={state.results} />
+            <DataTable columns={createColumns(spec?.resourceForName(resourceId))} data={state.resources.map((resource) => resource.properties)} />
         </div>
     );
 }
