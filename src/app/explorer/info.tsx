dd132ff79af9be6e89857723670ba879b421b32f
Alex Stephen <astephen2@gmail.com>
2026-04-04 18:17:27 -0700

    decouple info component from info page

--- src/app/explorer/info.tsx
+++ src/app/explorer/info.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Spinner } from "@/components/ui/spinner";
 import { ResourceInstance } from "@/state/fetch";
 import { ResourceSchema } from "@/state/openapi";
 import { useAppSelector } from "@/hooks/store";
 import { selectChildResources, selectHeaders } from "@/state/store";
-import ResourceListPage from "./resource_list";
-import { CustomMethodComponent } from "@/components/custom_method";
+import { ResourceInfo } from "@/components/info/info";
 
 type ResourceProperties = {
   path: string;
@@ -55,68 +52,11 @@ export default function InfoPage(props: InfoPageProps) {
       .then((instance) => setState(instance));
   }, [params, props.resource, parentParams, headers]);
 
-  const properties = useMemo(() => {
-    if (state?.properties) {
-      const results = [];
-      for (const [key, value] of Object.entries(
-        state.properties as ResourceProperties,
-      )) {
-        results.push(
-          <p key={key}>
-            <b>{key}:</b> {value}
-          </p>,
-        );
-      }
-      return results;
-    }
-    return <Spinner />;
-  }, [state]);
-
-  const customMethods = useMemo(() => {
-    return props.resource.customMethods();
-  }, [props.resource]);
-
   return (
-    <div className="space-y-6">
-      {/* Resource Instance card. */}
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            {(state?.properties as ResourceProperties)?.path}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>{properties}</CardContent>
-      </Card>
-
-      {/* Custom Methods */}
-      {state && customMethods.length > 0 && (
-        <Card>
-          <CardHeader>
-            <CardTitle>Custom Methods</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            {customMethods.map((customMethod) => (
-              <CustomMethodComponent
-                key={customMethod.name}
-                resourceInstance={state}
-                customMethod={customMethod}
-              />
-            ))}
-          </CardContent>
-        </Card>
-      )}
-
-      {/* Listing Child Resources */}
-      {childResources.map((childResource) => (
-        <Card key={childResource.singular_name}>
-          <CardHeader>
-            <CardTitle>{childResource.plural_name}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <ResourceListPage resource={childResource} />
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+    <ResourceInfo
+      resource={props.resource}
+      resourceInstance={state}
+      childResources={childResources}
+    />
   );
 }
